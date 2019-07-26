bc6fa46b8fac094db2dd6b558bce257b1b7466da
Alexandra <alexandrita@ukr.net>
2019-07-26 08:22:00 +0300

    Add

---
+++ script.js
@@ -0,0 +1,20 @@
+    var tableStyle = document.getElementById('table-style');
+
+    var selectedElement;
+
+    tableStyle.onclick = function(event) {
+      var target = event.target;
+        if (target.tagName == 'TD') {
+          setRedlight(target);
+          return;
+        }
+        target = target.parentNode;
+    }
+
+    function setRedlight(node) {
+      if (selectedElement) {
+        selectedElement.classList.remove('redlight');
+      }
+      selectedElement = node;
+      selectedElement.classList.add('redlight');
+    }
