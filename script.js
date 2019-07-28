d8dc070d64b641ea55aaa45ec3f94a6106fe6ecb
Alexandra <alexandrita@ukr.net>
2019-07-28 14:16:06 +0300

    Add

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-    var tableStyle = document.getElementById('table-style');
+function ClickSquare(){
+  var tableStyle = document.getElementById('table-style');
+  var selectedElement;
 
-    var selectedElement;
-
-    tableStyle.onclick = function(event) {
-      var target = event.target;
-        if (target.tagName == 'TD') {
-          setRedlight(target);
-          return;
-        }
-        target = target.parentNode;
-    }
-
-    function setRedlight(node) {
-      if (selectedElement) {
-        selectedElement.classList.remove('redlight');
+  tableStyle.onclick = function(event) {
+    var target = event.target;
+      if (target.tagName == 'TD') {
+        setRedlight(target);
+        return;
       }
-      selectedElement = node;
-      selectedElement.classList.add('redlight');
+    target = target.parentNode;
+  }
+
+  function setRedlight(node) {
+    if (selectedElement) {
+      selectedElement.classList.remove('redlight');
     }
+    selectedElement = node;
+    node.classList.add('redlight');
+  } 
+}
+
+ClickSquare();   
